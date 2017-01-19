e85903a3f8b82537eb96eb34ea6a36ce7b56f0c1
Jeff Andrews <jeff.andrews@carpages.ca>
2017-01-19 13:36:09 -0500

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,23 @@
+const eslint = require( 'eslint' );
+const test = require( 'ava' );
+const path = require( 'path' );
+
+const defaultLinter = new eslint.CLIEngine({
+  configFile: path.join( __dirname, '..', '.eslintrc' )
+});
+
+const reactLinter = new eslint.CLIEngine({
+  configFile: path.join( __dirname, '..', 'lib/react', '.eslintrc' )
+});
+
+test( 'test default linting config', t => {
+  t.plan( 1 );
+  const result = defaultLinter.executeOnFiles([ path.join( __dirname, 'fixtures/default.js' ) ]);
+  t.is( result.results[0].messages[0].message, 'There must be a space inside this paren.' );
+});
+
+test( 'test react linting config', t => {
+  t.plan( 1 );
+  const result = reactLinter.executeOnFiles([ path.join( __dirname, 'fixtures/react.js' ) ]);
+  t.is( result.results[0].messages[0].message, 'Missing semicolon.' );
+});
